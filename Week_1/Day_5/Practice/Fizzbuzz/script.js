bd8a694dcb46f2cfdb82c8795ca619bfff010112
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-08 13:19:11 +0100

    Fizzbuzz

--- Week_1/Day_5/Practice/Fizzbuzz/script.js
+++ Week_1/Day_5/Practice/Fizzbuzz/script.js
@@ -1,24 +1,9 @@
-1
-2
-"Fizz"
-4
-"Buzz"
-"Fizz"
-7
-8
-"Fizz"
-"Buzz"
-11
-"Fizz"
-13
-14
-"FizzBuzz"
-
-for(i=1;i<101;i++){
-
-}
-
-if (i % 3 == 0){
-
-
-}
+for(i=1;i<101;i++)
+{
+    if (i%3==0 &&i%5==0)
+        console.log ("FizzBuzz")
+    else if (i%5==0)
+        console.log ("Buzz")
+    else if (i%3==0) 
+        console.log ("Fizz")
+    else console.log (i)}
