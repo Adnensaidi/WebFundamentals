8aaa6503a36725d0398bc78ade720805967eea1f
Adnen Saidi <adnensaidi8294@gmail.com>
2024-10-10 21:52:43 +0100

    Loop Challenges

--- Week_1/Day_5/Practice/Loop Challenges/scripts.js
+++ Week_1/Day_5/Practice/Loop Challenges/scripts.js
@@ -1,35 +1,36 @@
 // Print odds 1-20
-for (var i=1;i<=20;i++){
-    if (i% 2!=0)
-        {console.log (i)}
+
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
 }
 
 
 // Decreasing Multiples of 3
-for (var i=100;i>=0;i--){
-    if (i%3==0)
-        {console.log (i)}
+for (var i = 100; i >= 0; i--) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
 }
-
 
 // Print the sequence
-for (var i=4;i>=-3,5;i=i-1.5)
-    {console.log (i)}
+for (var i = 4; i >= -3.5; i -= 1.5) {
+    console.log(i);
+}
+
 
 //Sigma 
-var s=0
-for (var i=1;i>100;i++)
-    {
-    if (s=s+i)
-        {console.log (s)}
+var s = 0;
+for (var i = 1; i <= 100; i++) {
+    s += i; 
 }
+console.log(s);
 
 // Factorial
 
-var s=0
-for(var i=1;i>12;i*1)
-    {
-    if (s=s*i)
-    
-    {console.log (s)}
+var s = 1; 
+for (var i = 1; i <= 12; i++) { 
+    s *= i; 
     }
+    console.log(s);
